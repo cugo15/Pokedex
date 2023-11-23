f9e78874d0cb816bf633374df538515bded44fd0
Cumhur Günay <cugo2435@gmail.com>
2023-11-23 06:28:11 +0300

    recycler adapter created

--- app/src/main/java/com/cugocumhurgunay/pokedex/presentation/view/adapter/PokeAdapter.kt
+++ app/src/main/java/com/cugocumhurgunay/pokedex/presentation/view/adapter/PokeAdapter.kt
@@ -1,4 +1,35 @@
 package com.cugocumhurgunay.pokedex.presentation.view.adapter
 
-class PokeAdapter {
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.cugocumhurgunay.pokedex.databinding.PokeRowBinding
+
+class PokeAdapter () : RecyclerView.Adapter<PokeAdapter.PokeHolder>(){
+
+    private var pokeList: List<String> = listOf()
+    inner class PokeHolder(val binding: PokeRowBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PokeHolder {
+        val itemBinding = PokeRowBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PokeHolder(itemBinding)
+    }
+
+    override fun getItemCount(): Int {
+        return pokeList.size
+    }
+
+    override fun onBindViewHolder(holder: PokeHolder, position: Int) {
+        val poke = pokeList[position]
+        val pokeRow = holder.binding
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun updatePokeList(item: List<String>) {
+        item.let {
+            pokeList = item
+        }
+        notifyDataSetChanged()
+    }
 }
